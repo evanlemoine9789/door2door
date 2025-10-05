3e2c8d131af0ca92aca1b976026aa93f1687be96
Evan Lemoine <evanlemoine@Evans-MacBook-Pro-2.local>
2025-10-05 10:19:57 -0400

    Fix map loading in production

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // External packages for server components
-  serverExternalPackages: ['leaflet', 'react-leaflet']
 }
 
 module.exports = nextConfig
